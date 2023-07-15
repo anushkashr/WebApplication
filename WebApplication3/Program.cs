79e918b3432177dd6b5c1ada5fa4a1298445f1b1
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-07-14 22:41:01 -0500

    removed wwwroot

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -3,7 +3,7 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
-
+app.UseStaticFiles(); //used for all the functionalities present in wwwroot folder
 app.UseRouting();
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
