d00c4010695675db0012b555562d310584e24ce5
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-07-14 22:37:57 -0500

    Added bootstrap and jquery

---
+++ WebApplication3/Properties/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Properties.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
