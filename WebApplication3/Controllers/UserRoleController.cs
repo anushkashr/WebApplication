e7f71889de37b12435b60aac6a57586ce5b56bd4
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-07-18 21:44:52 -0500

    Added ViewImport and ViewStart

---
+++ WebApplication3/Controllers/UserRoleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Data;
+
+namespace WebApplication3.Controllers
+{
+    public class UserRoleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRoleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
