8828a54eb6e1feea0d2a25196a3b1ec1423acaa7
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-07-25 18:28:28 -0500

    Added Confirm password functionality and used Client side libraries to prevent postback

---
+++ WebApplication3/Helper/DobValidationAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication3.Helper
+{
+    public class DobValidationAttribute: ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime _dob=Convert.ToDateTime(value);
+            if(_dob.Date==DateTime.Today.Date)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- WebApplication3/ViewModel/UserViewModel.cs
+++ WebApplication3/ViewModel/UserViewModel.cs
@@ -1,6 +1,8 @@
-﻿namespace WebApplication3.ViewModel
+﻿using WebApplication3.Helper;
+
+namespace WebApplication3.ViewModel
 {
-    public class UserViewModel
+    public class UserViewModel: IValidatableObject
     {
         public int UserID { get; set; }
 
@@ -17,18 +19,46 @@
         [Display(Name = "User Email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-     
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+
 
         [DataType(DataType.Date)]
+        [DobValidationAttribute(ErrorMessage="Date of Birth cannot be today")]
+        [Display(Name = "Date of Birth")]
         public string Dob { get; set; }
 
         [Required]
         [Range(18,50, ErrorMessage ="Age must be between 18 and 50")]
        
-        public string Age { get; set; }
+        public int Age { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="Confirmation of password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password must match")]
+        public string ConfirmPassword { get; set; }
+
+        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+        {
+            //we can specify our custom logic here
+            DateTime _dob = Convert.ToDateTime(Dob).Date;
+            if(_dob > DateTime.Today)
+            {
+                //we can do it through a list here but then we have to insert data, store and then retrieve 
+                //so we use yield because it returns values one by one
+                yield return new ValidationResult("DOB cannot be greater than today");
+            }
+            if(_dob < DateTime.Today.AddYears(-50))
+            {
+                yield return new ValidationResult("DOB cannot be too past");
+            }
+            if(Age == 20)
+            {
+                yield return new ValidationResult("Age cannot be 20");
+            }
+        }
     }
 }
