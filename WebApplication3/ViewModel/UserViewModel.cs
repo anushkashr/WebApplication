4b78b23061d6bc30c0698225aff1b5e5893f2a40
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-07-25 14:31:21 -0500

    Added Data annotations

--- WebApplication3/ViewModel/UserViewModel.cs
+++ WebApplication3/ViewModel/UserViewModel.cs
@@ -5,25 +5,30 @@
         public int UserID { get; set; }
 
       
-        [Required]
-
+        [Required(ErrorMessage="First Name is required field")]
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
-     
 
 
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
      
         [Required]
-
-
+        [Display(Name = "User Email")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
      
         [Required]
-
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [DataType(DataType.Date)]
+        public string Dob { get; set; }
 
-        public string Address { get; set; }
+        [Required]
+        [Range(18,50, ErrorMessage ="Age must be between 18 and 50")]
+       
+        public string Age { get; set; }
 
     }
 }
