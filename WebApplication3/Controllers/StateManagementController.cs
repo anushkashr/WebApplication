2cb6de90070b7bd95200b3552c86df2940176dd4
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-07-30 16:50:36 -0500

    ViewData Concepts

--- WebApplication3/Controllers/StateManagementController.cs
+++ WebApplication3/Controllers/StateManagementController.cs
@@ -19,9 +19,16 @@ namespace WebApplication3.Controllers
 
             //view bata counter directly access garna mildaina so, using ViewBag
             //so that we can pass counter(can be a message or data) to view from controller
-            ViewBag.UserRole = _context.UserRoles;
+            //doesn't need typecasting
+            //ViewBag.UserRole = _context.UserRoles;
 
-            ViewBag.SingleUserRole = _context.UserRoles.FirstOrDefault();
+            //ViewBag.SingleUserRole = _context.UserRoles.FirstOrDefault();
+
+            //used to throw message to view
+            //has a key value pair syntax
+            //needs typecasting
+            ViewData["UserRole"] = _context.UserRoles;
+
             return View();
         }
     }
