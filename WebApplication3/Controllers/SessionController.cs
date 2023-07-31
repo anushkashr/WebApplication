43f10079684a5555cbfb937188c279e40af4185f
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-07-30 19:36:19 -0500

    Maintaining session through HttpContextAccessor

---
+++ WebApplication3/Controllers/SessionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
+
+namespace WebApplication3.Controllers
+{
+    public class SessionController : Controller
+    {
+        int counter = 0;
+        public IActionResult SessionIndex()
+        {
+            if (HttpContext.Session.GetInt32("CounterKey") != null)
+            {
+                counter = (int)HttpContext.Session.GetInt32("CounterKey");
+            }
+           
+            counter++;
+            TempData["Counter"] = counter;
+
+            HttpContext.Session.SetInt32("CounterKey", counter);
+            return View();
+        }
+    }
+}
--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -9,11 +9,20 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("myconnection")));
 
+//using Dependency injection 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+//to build sessions
+builder.Services.AddSession();
+
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 app.UseStaticFiles(); //used for all the functionalities present in wwwroot folder
+app.UseSession(); //for sessions
 app.UseRouting();
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+//the above commands run step by step 
